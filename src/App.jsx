c673cd3cf0d59d59b1a1c405a0837602dc5d1694
apache0505 <jiayulutsk@ukr.net>
2022-12-30 13:33:38 +0200

    das

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import React, { useEffect, useParams } from 'react';
 import { Provider } from 'react-redux';
 import Flights from './features/flights/components/Flights';
 import store from './store';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="*">
-          <Provider store={store}>
-            <Flights />
-          </Provider>
-        </Route>
-      </Switch>
+      <Provider store={store}>
+        <Flights />
+      </Provider>
     </Router>
   );
 };
